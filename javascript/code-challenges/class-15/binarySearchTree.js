2260cfc0b08b4c29ac5eee08e9d0dd7c1dbc53e3
mamoun kamal alshishani <mamoun.alshishani@yahoo.com>
2021-09-03 19:04:27 +0300

    first

---
+++ javascript/code-challenges/class-15/binarySearchTree.js
@@ -0,0 +1,120 @@
+const Node =require('./node')
+const BinaryTree =require('./binerytree')
+
+class BinarySearchTree extends BinaryTree{
+
+constructor(){
+    super()
+
+this.root = null;
+
+}
+add(value){
+
+const node = this.root;
+if(node === null){
+    this.root = new Node(value);
+    return;
+}else {
+    const searchTree = function(node){
+        if(value < node.value){
+            if(node.left ===null){
+                node.left = new Node(value)
+                return;
+            }else if(node.left !==null){
+                return searchTree(node.left);
+            }
+        }else if(value> node.value){
+            if(node.right === null){
+                node.right = new Node(value);
+                return;
+            }else if(node.right !==null){
+                return searchTree(node.right);
+            }
+        }else{ return null;
+        }
+    }
+    return searchTree(node)
+}
+
+
+}
+// add(value){
+
+// const node=this.root;
+// if(node === null){
+
+// this.root = new Node(value)
+
+// return;
+// }else{
+
+// const searchTree = function(node){
+
+//  if(value<node.value){
+
+// if(node.left ===null){
+
+//     node.left =new Node(value);
+//     return;
+
+// }else if(node.left !==null){
+//     return searchTree(node.left);
+
+// }else if(value>node.value){
+
+// if(node.right ===null){
+
+// node.right = new Node(value)
+// return;
+// }else if(node.right !==null){
+
+//     return searchTree(node.right)
+// }else{
+
+//     return null;
+// }
+
+// }
+
+//  }
+//  return searchTree(node)
+// }
+
+
+
+// }
+contains(value){
+
+   let current = this.root;
+   while (current){
+if(value ===current.value){
+    return true;
+}
+if(value<current.value){
+    current =current.left;
+
+}else{current=current.right}
+
+   }return false;
+}
+
+}
+
+
+const bst = new BinarySearchTree()
+console.log(bst.add(1));
+bst.add(1)
+bst.add(2)
+bst.add(3)
+bst.add(4)
+bst.add(5)
+bst.add(6)
+bst.add(7)
+bst.add(8)
+bst.add(9)
+
+console.log(bst.contains(1));
+console.log(bst.contains(2));
+console.log(bst.contains(20));
+module.exports= BinarySearchTree;
---
+++ javascript/code-challenges/class-15/readme-15.md
@@ -0,0 +1,17 @@
+# Stack and a Queue Implementation
+
+
+## code test :
+![](test-class-15.jpg)
+
+
+
+
+Complexity Analysis:
+Time Complexity:
+Push operation : O(1).
+
+Pop operation : O(N).
+
+Auxiliary Space: O(N).
+
